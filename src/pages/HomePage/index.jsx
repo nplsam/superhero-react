5a0c0f5a47340971da87db7440e2fecd38a6bcc2
nplsam <psammerrick@gmail.com>
2023-09-07 12:48:24 +0100

    added some styling to the home page and nav bar

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div className="home-page">
         <div className="cover-image">
-            <h1>Welcome to the Superhero App</h1>
+            <h1>Superheroes</h1>
             <p>Explore superheroes from the Marvel and DC universe!</p>
         </div>
     </div>
