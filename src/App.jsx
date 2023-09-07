4c2e1a7ca0a6b4526b30cde49451e7bff47e0c7b
Barbara Fajardo <barbarafajardo92@gmail.com>
2023-09-07 10:55:56 +0100

    Add superheroes page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import * as Pages from './pages'
-import { Header } from './components'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import * as Pages from "./pages";
+import { Header } from "./components";
 
 const App = () => {
   return (
-      <Routes>
-          <Route path="/" element={<Header />}>
-          <Route index element={<Pages.HomePage />}/>
-          </Route>
-      </Routes>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Pages.HomePage />} />
+        <Route path="/superheroes" element={<Pages.SuperHeroesPage />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/SuperHeroesPage/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SuperHeroesPage = () => {
+  return <div>SuperHeroPage</div>;
+};
+
+export default SuperHeroesPage;
